9fe71c102429a2326afd89d2a8184fd6c226a528
DevBloo <devchanghwan@gmail.com>
2021-11-13 14:17:27 +0900

    add: index.html

--- goglekaap/__init__.py
+++ goglekaap/__init__.py
@@ -20,13 +20,13 @@ def create_app():
     @app.before_first_request
     def before_first_request():
         app.logger.info('BEFORE_FIRST_REQUEST')
-    
+
     @app.after_request
     def after_request(response):
         app.logger.info('AFTER REQEUST')
         # after request는 response를 리턴해야 함
         return response
-    
+
     # teardown은 Reqeust가 끝날 때 실행된다.
     @app.teardown_request
     def teardown_request(exception):
--- goglekaap/app.py
+++ goglekaap/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 if __name__ == '__main__':
     print('run')
     app.run(debug=True)
